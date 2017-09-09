b8bef62d0b2f4c1dc79b58c90863784d9e5cd9a8
pbrudny <pbrudny@gmail.com>
2017-09-09 13:10:51 +0200

    Add colors

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 
 function getOSinfo() {
     var type = os.type();
@@ -11,12 +12,12 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log(('System:').gray, type);
+    console.log(('Release:').red, release);
+    console.log(('CPU model:').blue, cpu);
+    console.log(('Uptime: ~').green, (uptime / 60).toFixed(0), 'min');
+    console.log(('User name:').yellow, userInfo.username);
+    console.log(('Home dir:').grey, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
